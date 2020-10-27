35a6cfb8689b6150cf63bf9606875c649c9d7c39
Jinesh Shah <jinsha@microsoft.com>
2020-10-27 14:59:17 +0530

    initial commit

---
+++ profile.ps1
@@ -0,0 +1,14 @@
+﻿if(Get-Module -ListAvailable Azure) {
+    $subscription = Get-AzureSubscription -Current -ErrorAction SilentlyContinue
+    if(-not $subscription) {
+        $subscriptionMessage = "There is actually no selected Azure subscription."
+    }
+    else {
+        $subscriptionMessage = ("Actually targeting Azure subscription: {0} - {1}." -f $subscription.SubscriptionName, $subscription.SubscriptionId)
+    }
+    $azureModule = Get-Module AzureRm.Profile
+    if($azureModule) {
+        Write-Verbose ('Using Azure PowerShell v{0}' -f $azureModule.Version) -Verbose
+    }
+    Write-Verbose $subscriptionMessage -Verbose
+}
